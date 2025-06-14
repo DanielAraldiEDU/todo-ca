0aecdbbbc4124e2f7d00cf8d475e338ec16dc19d
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-06-14 18:10:24 -0300

    feat: add main structure

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <p>Hello World!</p>;
+  return (
+    <main className='flex flex-col w-full h-screen items-center p-16 overflow-y-auto'>
+      <div className='flex flex-col w-full max-w-4xl gap-8'></div>
+    </main>
+  );
 }
 
 export default App;
