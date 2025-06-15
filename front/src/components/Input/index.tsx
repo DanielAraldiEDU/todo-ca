8948297dfaabdd5b585643f5c65560450f08d13b
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-06-15 13:47:14 -0300

    feat: add `p` tag when `Input` is read only and swap border style to ring

--- front/src/components/Input/index.tsx
+++ front/src/components/Input/index.tsx
@@ -2,7 +2,7 @@ import type { ChangeEvent } from 'react';
 import type { InputProps } from '../../@types';
 
 export function Input(props: InputProps) {
-  const { variant = 'text', onChange, ...rest } = props;
+  const { variant = 'text', onChange, readOnly, value, ...rest } = props;
 
   const isText = variant === 'text';
   const isTextarea = variant === 'textarea';
@@ -15,30 +15,42 @@ export function Input(props: InputProps) {
 
   return (
     <>
-      {isText && (
-        <input
-          required
-          placeholder='Título'
-          type='text'
-          className='flex w-full h-14 !px-4 !py-1 border-2 border-neutral-100 rounded-lg outline-none text-xl text-neutral-100 shadow-neutral-100 shadow-sm hover:opacity-95 hover:read-only:opacity-100 hover:disabled:opacity-80 disabled:cursor-not-allowed disabled:opacity-80 placeholder:text-neutral-500 read-only:h-11 read-only:border-none read-only:shadow-none read-only:text-neutral-300'
-          onChange={onChangeText}
-          min={1}
-          minLength={1}
-          {...rest}
-        />
-      )}
+      {isText &&
+        (readOnly ? (
+          <p className='flex w-full min-h-14 items-center p-4 text-xl text-neutral-300'>
+            {value}
+          </p>
+        ) : (
+          <input
+            required
+            placeholder='Título'
+            type='text'
+            className='flex w-full h-14 px-4 py-1 ring-1 ring-neutral-100 rounded-lg outline-none text-xl text-neutral-100 focus:ring-2 focus-within:right-2 hover:opacity-90 hover:disabled:opacity-80 disabled:cursor-not-allowed disabled:opacity-80 placeholder:text-neutral-500'
+            onChange={onChangeText}
+            min={1}
+            minLength={1}
+            value={value}
+            {...rest}
+          />
+        ))}
 
-      {isTextarea && (
-        <textarea
-          required
-          placeholder='Anotações'
-          rows={5}
-          minLength={1}
-          className='flex w-full h-24 min-h-20 max-h-96 !p-4 border-2 border-neutral-100 rounded-lg outline-none text-xl text-neutral-100 shadow-neutral-100 shadow-sm hover:opacity-95 hover:read-only:opacity-100 hover:disabled:opacity-80 disabled:cursor-not-allowed disabled:opacity-80 placeholder:text-neutral-500 read-only:h-16 read-only:min-h-16 read-only:border-none read-only:shadow-none read-only:text-neutral-300 read-only:resize-none'
-          onChange={onChangeText}
-          {...rest}
-        />
-      )}
+      {isTextarea &&
+        (readOnly ? (
+          <p className='flex w-full min-h-24 p-4 text-xl text-neutral-300'>
+            {value}
+          </p>
+        ) : (
+          <textarea
+            required
+            placeholder='Anotações'
+            rows={5}
+            minLength={1}
+            className='flex w-full h-24 min-h-20 max-h-96 p-4 ring-1 ring-neutral-100 rounded-lg outline-none text-xl text-neutral-100 focus:ring-2 focus-within:right-2 hover:opacity-90 hover:disabled:opacity-80 disabled:cursor-not-allowed disabled:opacity-80 disabled:resize-none placeholder:text-neutral-500'
+            onChange={onChangeText}
+            value={value}
+            {...rest}
+          />
+        ))}
     </>
   );
 }
